d788ca0a07a7935a2f56e185c246552493221c12
pepsinmentos <pieter.roodt@gmail.com>
2016-03-13 17:02:51 +0200

    added rest calls with json parsing

--- ForTwo.Client/src/com/example/fortwo/client/ChatActivity.java
+++ ForTwo.Client/src/com/example/fortwo/client/ChatActivity.java
@@ -2,9 +2,15 @@ package com.example.fortwo.client;
 
 import java.util.Random;
 
+import com.example.fortwo.client.constants.LoggingConstants;
+import com.example.fortwo.client.model.Chat;
+import com.fortwo.client.services.ChatService;
+
 import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,42 +22,69 @@ import android.widget.Toast;
 
 public class ChatActivity extends Activity {
 
+	private ChatService chatService;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_chat);
+
+		chatService = new ChatService();
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.chat, menu);
-		
+
 		addGetChatHandler();
 		return true;
 	}
-	
-	private void addGetChatHandler(){
-	
-		Button btn = (Button)findViewById(R.id.get_chat_button);
+
+	private void addGetChatHandler() {
+
+		Button btn = (Button) findViewById(R.id.get_chat_button);
 		btn.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View view) {
-					Toast.makeText(ChatActivity.this, "Getting chats from server", Toast.LENGTH_LONG).show();
-					View linearLayout = findViewById(R.id.chat_view);
-					
-					TextView valueTV = new TextView(ChatActivity.this);
-					valueTV.setText("Hallo hallow");
-					Random rand = new Random();
-					valueTV.setId(rand.nextInt());
-					valueTV.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
-					
-					((LinearLayout)linearLayout).addView(valueTV);
+				Toast.makeText(ChatActivity.this, "Getting chats from server", Toast.LENGTH_LONG).show();
+
+				AsyncTask<Void, Void, Chat> getChatTask = new AsyncTask<Void, Void, Chat>() {
+
+					@Override
+					protected Chat doInBackground(Void... params) {
+						return chatService.getChat();
+					}
+
+					@Override
+					protected void onPostExecute(Chat result) {
+						Log.d(LoggingConstants.LOGGING_TAG, "got chat successfully");
+						try {
+
+							View linearLayout = findViewById(R.id.chat_view);
+
+							TextView valueTV = new TextView(ChatActivity.this);
+							valueTV.setText(result.getMessage());
+							Random rand = new Random();
+							valueTV.setId(rand.nextInt());
+							valueTV.setLayoutParams(
+									new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+
+							((LinearLayout) linearLayout).addView(valueTV);
+						} catch (Exception e) {
+							Log.e("PR", e.toString());
+						}
+
+					}
+
+				};
 				
+				getChatTask.execute();
+
 			}
 		});
-		
+
 	}
 
 	@Override
--- ForTwo.Client/src/com/fortwo/client/services/ChatService.java
+++ ForTwo.Client/src/com/fortwo/client/services/ChatService.java
@@ -1,5 +1,57 @@
 package com.fortwo.client.services;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.json.simple.parser.JSONParser;
+
+import com.example.fortwo.client.model.Chat;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import android.util.Log;
+
 public class ChatService {
 
+	public ChatService()
+	{
+		
+	}
+	
+	public Chat getChat()
+	{
+		URL url;
+		try {
+			url = new URL("http://10.0.2.2:8080/server/webapi/chat");
+			HttpURLConnection con = (HttpURLConnection)url.openConnection();
+			con.setRequestMethod("GET");
+			con.setRequestProperty("Accept", "application/json");
+			
+			BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			StringBuilder builder = new StringBuilder();
+			String line;
+			while((line = reader.readLine()) != null)
+			{
+				builder.append(line);
+				
+			}
+			
+			ObjectMapper mapper = new ObjectMapper();
+			
+			Chat chat = mapper.readValue(builder.toString(), Chat.class);
+			
+			return chat;
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			Log.e("PR", e.toString());
+		}
+		
+		
+		
+		return new Chat();
+	}
+	
+	
 }
