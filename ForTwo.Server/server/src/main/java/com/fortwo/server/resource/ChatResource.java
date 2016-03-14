368ff605eab2a5c7bf051588c7d8e0c088bd2845
pepsinmentos <pieter.roodt@gmail.com>
2016-03-14 22:34:30 +0200

    added post method

--- ForTwo.Server/server/src/main/java/com/fortwo/server/resource/ChatResource.java
+++ ForTwo.Server/server/src/main/java/com/fortwo/server/resource/ChatResource.java
@@ -1,6 +1,7 @@
 package com.fortwo.server.resource;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -13,6 +14,11 @@ public class ChatResource {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Chat getChats(){
-		return new Chat("Hello", 1, 1);
+		return new Chat("hard coded chats", 1, 1);
+	}
+	
+	@POST
+	public void SendChat(Chat chat){
+		
 	}
 }
