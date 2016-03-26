6b497ca3f862ed6181e253c1efcb5e30eb7f316c
Jelligeth <ldaoyi@gmail.com>
2016-03-26 10:10:05 +0200

    finally got the push message client working in Android

--- ForTwo.Client/src/com/fortwo/client/services/ChatService.java
+++ ForTwo.Client/src/com/fortwo/client/services/ChatService.java
@@ -5,22 +5,59 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import org.json.simple.parser.JSONParser;
 
 import com.example.fortwo.client.model.Chat;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import android.os.AsyncTask;
 import android.util.Log;
 
-public class ChatService {
+import microsoft.aspnet.signalr.client.Platform;
+import microsoft.aspnet.signalr.client.SignalRFuture;
+import microsoft.aspnet.signalr.client.http.android.AndroidPlatformComponent;
+import microsoft.aspnet.signalr.client.hubs.HubConnection;
+import microsoft.aspnet.signalr.client.hubs.HubProxy;
+import microsoft.aspnet.signalr.client.hubs.SubscriptionHandler;
 
+public class ChatService {
+	HubProxy hub;
 	public ChatService()
 	{
+		Platform.loadPlatformComponent(new AndroidPlatformComponent());
 		
+		AsyncTask<Void,Void,Void> v = new AsyncTask<Void,Void,Void>(){
+		  
+		@Override
+		protected Void doInBackground(Void... arg0) {
+			// TODO Auto-generated method stub
+			String host = "http://10.0.2.2:8081/signalr";
+			HubConnection connection = new HubConnection(host);
+			Log.d("PR", "Go connection");
+			 hub = connection.createHubProxy("ChatHub");
+				Log.d("PR", "Go hub");	
+			SignalRFuture<Void> awaitConnection = connection.start();
+			Log.d("PR", "Go connection start");
+			try{
+				awaitConnection.get();	
+				Log.d("PR", "Go connection get");  
+			}
+			catch(Exception ex){
+				Log.e("PR", ex.toString());
+			}
+			
+			hub.subscribe(ChatService.this);
+			return null;
+		}
+		
+		
+		};
+		
+		v.execute();
 	}
 	
 	public Chat getChat()
 	{
+		/*
 		URL url;
 		try {
 			url = new URL("http://10.0.2.2:8080/server/webapi/chat");
@@ -49,13 +86,18 @@ public class ChatService {
 		}
 		
 		
-		
+		*/
 		return new Chat();
 	}
 	
 	public void sendChat(Chat chat){
-		
+		Log.d("PR", "Sending chat now");
+		hub.invoke("SendMessage", chat.getMessage());
 	}
 	
+	
+	public void addMessage(String m){
+		Log.d("PR", "I GOT THE FUCKING MESSAGE: " + m);
+	}
 	
 }
